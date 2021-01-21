dd0b083818faac9a4983188d46affda1e8b34a02
Felipe-BP <gvv.fel@gmail.com>
2021-01-21 15:36:44 -0300

    feat: scaffolding cli package

---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,24 @@
+import { cyan } from 'chalk';
+import { textSync } from 'figlet';
+import yargs from 'yargs';
+
+function main() {
+    console.clear();
+
+    console.log(
+        cyan(
+            textSync('fastty-cli', { horizontalLayout: 'full' }),
+        ),
+    );
+
+    console.log('\n\nWelcome to Fastty-CLI, this will help you to easy integrate your server and client application.\n\n');
+
+    const args = yargs.options({
+        'source': { alias: 's', string: true, demandOption: true, description: 'Path that fastty loops recursively searching for pattern' },
+        'dest': { alias: 'd', string: true, demandOption: true, description: 'Path that fastty will export code integration' },
+    }).argv;
+
+    console.log(args);
+}
+
+main();
