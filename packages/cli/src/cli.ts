269d4b5ceec7c906a383691d86fb8880ae8a761c
Felipe-BP <gvv.fel@gmail.com>
2021-01-27 23:06:56 -0300

    style: formatting files with prettier

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { cyan } from 'chalk';
 import { textSync } from 'figlet';
 import yargs from 'yargs';
@@ -5,17 +6,25 @@ import yargs from 'yargs';
 function main() {
     console.clear();
 
+    console.log(cyan(textSync('fastty-cli', { horizontalLayout: 'full' })));
+
     console.log(
-        cyan(
-            textSync('fastty-cli', { horizontalLayout: 'full' }),
-        ),
+        '\n\nWelcome to Fastty-CLI, this will help you to easy integrate your server and client application.\n\n',
     );
 
-    console.log('\n\nWelcome to Fastty-CLI, this will help you to easy integrate your server and client application.\n\n');
-
     const args = yargs.options({
-        'source': { alias: 's', string: true, demandOption: true, description: 'Path that fastty loops recursively searching for pattern' },
-        'dest': { alias: 'd', string: true, demandOption: true, description: 'Path that fastty will export code integration' },
+        source: {
+            alias: 's',
+            string: true,
+            demandOption: true,
+            description: 'Path that fastty loops recursively searching for pattern',
+        },
+        dest: {
+            alias: 'd',
+            string: true,
+            demandOption: true,
+            description: 'Path that fastty will export code integration',
+        },
     }).argv;
 
     console.log(args);
