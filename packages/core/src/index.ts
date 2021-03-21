6b23d29639b3de08f265b7aa3f3c07ed1873b490
Felipe-BP <gvv.fel@gmail.com>
2021-03-21 00:16:54 -0300

    refactor: removed unnecessary exporter codes
    - Exported code now will be done through schematics

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,27 +1,10 @@
 import ts from 'typescript';
 import { join } from 'path';
-import { writeFileSync } from 'fs';
 
 import { nestjsParser } from './parsers';
-import { angularExporter } from './exporters';
-import { Document } from './interfaces/document.interface';
 
 export function parseSource(sourceFile: ts.SourceFile, checker: ts.TypeChecker) {
     return nestjsParser(sourceFile, checker);
-}
-
-export function exportSource(doc: Document) {
-    const resultFile = ts.createSourceFile(
-        'someFileName.ts',
-        '',
-        ts.ScriptTarget.Latest,
-        /*setParentNodes*/ false,
-        ts.ScriptKind.TS,
-    );
-    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-    const result = printer.printList(ts.ListFormat.MultiLine, angularExporter(doc), resultFile);
-
-    writeFileSync(resultFile.fileName, result, 'utf8');
 }
 
 const fileNames = [join(__dirname, '/resource/cat.controller.ts')];
@@ -39,9 +22,6 @@ for (const sourceFile of program.getSourceFiles()) {
     }
 
     if (!sourceFile.isDeclarationFile && isRootSourceFile(sourceFile.fileName)) {
-        const document = parseSource(sourceFile, program.getTypeChecker());
-        if (document) {
-            exportSource(document);
-        }
+        parseSource(sourceFile, program.getTypeChecker());
     }
 }
