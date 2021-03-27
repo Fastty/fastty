9f05818faf82c5642f1d997287df05d64ca71bf6
Felipe-BP <gvv.fel@gmail.com>
2021-03-27 00:54:54 -0300

    refactor(schematics): organize schematic entrypoint
    - Fix change mergeStrategy to override if run again and template already exists on fs

--- packages/schematics/src/lib/angular/index.ts
+++ packages/schematics/src/lib/angular/index.ts
@@ -3,230 +3,26 @@ import {
     apply,
     applyTemplates,
     chain,
-    forEach,
+    MergeStrategy,
     mergeWith,
     move,
     Rule,
     SchematicContext,
+    SchematicsException,
     Tree,
     url,
 } from '@angular-devkit/schematics';
 
-import { ESLint } from 'eslint';
-
+import { runLint } from '../../rules';
 import { AngularServiceSchema } from './schema';
-import { Document } from '../../interfaces/document.interface';
-import { document } from './resource/document-resource';
+import { lowerCase, upperCase } from '../../utils';
+import { getEndpoint, buildBodyFromMember, buildMemberParameters } from '../../helpers';
 
-interface HttpClientParameters {
-    httpClientArguments: string[];
-    prefixCode?: string;
-}
-
-interface HttpQueryParams {
-    params: {
-        [param: string]: string | string[];
-    };
-}
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function schematics(_options: AngularServiceSchema): Rule {
-    function upperCase(value: string): string {
-        return value.toUpperCase();
-    }
-
-    function lowerCase(value: string): string {
-        return value.toLowerCase();
-    }
-
-    function buildMemberParameters(member: Document) {
-        let finalParameters: string = '';
-        if (member.constructors && member.constructors.length) {
-            const [{ parameters }] = member.constructors;
-            if (parameters && parameters.length) {
-                finalParameters = parameters
-                    .map(p => `${p.name}: ${p.type}`)
-                    .join(', ');
-            }
-        }
-        return finalParameters;
-    }
-    
-    function buildBodyFromMember(document: Document, member: Document): string {
-        const [decorator] = member?.decorators ?? [];
-        const httpMethod = decorator && decorator.name;
-        const memberEnpoint = getEndpoint(document, member);
-        const hasHttpParam = (endpoint: string): boolean => !!endpoint.trim().match(/\/\:\w+/g);
-        const hasQueryParam = (parameters: Document[]): boolean =>
-            parameters.some(p =>
-                p.decorators && p.decorators.some(d => d.name === 'Query')
-            )
-        const getBodyParam = (parameters: Document[]): Document | undefined =>
-            parameters.find(p =>
-                p.decorators && p.decorators.some(d => d.name === 'Body')
-            )
-
-        let baseMemberContent = 
-            `return this.http.${lowerCase(httpMethod)}<${member.returnType}>`;
-
-        if (member.constructors && member.constructors.length) {
-            let httpClientArguments: string[] = [`API_BASE + ${upperCase(strings.underscore(member.name))}`];
-            let prefixCode: string | undefined = undefined;
-            const [{ parameters }] = member.constructors;
-
-            if (parameters) {
-                if (hasHttpParam(memberEnpoint)) {
-                    ({ httpClientArguments, prefixCode } =
-                        parseWithHttpParam(parameters, member));
-
-                    if (prefixCode) {
-                        baseMemberContent = prefixCode + baseMemberContent;
-                    }
-
-                    if (hasQueryParam(parameters)) { // both route param and query param
-                        const parsedWithQueryParam =
-                            parseWithQueryParam(parameters, member.name, true);
-    
-                        httpClientArguments = httpClientArguments
-                            .concat(parsedWithQueryParam.httpClientArguments);
-
-                        if (parsedWithQueryParam.prefixCode) {
-                            baseMemberContent = parsedWithQueryParam.prefixCode + baseMemberContent;
-                        }
-                    }
-                } else if (hasQueryParam(parameters)) {
-                    ({ httpClientArguments, prefixCode } =
-                        parseWithQueryParam(parameters, member.name));
-
-                    if (prefixCode) {
-                        baseMemberContent = prefixCode + baseMemberContent;
-                    }
-                }
-
-                const bodyParameter = getBodyParam(parameters);
-
-                if (!!bodyParameter) {
-                    httpClientArguments.splice(1, 0, bodyParameter.name);
-                }
-
-                baseMemberContent += `(${httpClientArguments.join(', ')});`;
-            }
-        }
-
-        return baseMemberContent;
-    }
-
-    function parseWithQueryParam(parameters: Document[], memberName: string, hasRouteParam = false): HttpClientParameters {
-        const httpQueryParams = parameters
-            .filter(p => {
-                if (p.decorators
-                    && p.decorators.length
-                    && p.decorators.some(d => d.name === 'Query')) {
-                    return true;
-                }
-
-                return false;
-            })
-            .reduce<HttpQueryParams>((acc, curr) => {
-                const { arguments: args } = curr.decorators?.find(d => d.name === 'Query') as Document;
-                const [ { name: queryParamKey } ] = args as Document[];
-                const queryParamValue = curr.name;
-
-                acc = {
-                    params: {
-                        ...acc.params,
-                        [queryParamKey]: queryParamValue,
-                    }
-                };
-
-                return acc;
-            }, { params: {} });
-
-
-        const httpClientArguments: string[] = [`API_BASE + ${upperCase(strings.underscore(memberName))}`];
-        if (hasRouteParam) {
-            httpClientArguments.splice(0, 1, 'url');
-        }
-
-        if (httpQueryParams) {
-            httpClientArguments.push(
-                JSON.stringify(httpQueryParams).replace(/\"/g, '')
-            );
-        }
-
-        return { httpClientArguments };
-    }
-
-    function parseWithHttpParam(parameters: Document[], member: Document): HttpClientParameters {
-        const httpParams = parameters
-            .filter(p => {
-                if (p.decorators
-                    && p.decorators.length
-                    && p.decorators.some(d => d.name === 'Param')) {
-                    return true;
-                }
-
-                return false;
-            })
-            .map(p => {
-                let paramKey = p.name;
-                if (p.decorators && p.decorators[0].arguments) {
-                    paramKey = p.decorators[0].arguments[0].name;
-                }
-                return {
-                    name: p.name,
-                    paramKey,
-                };
-            });
-
-        let url = '';
-        if (httpParams && httpParams.length) {
-            url = `const url = API_BASE + ${upperCase(strings.underscore(member.name))}`;
-            for (const param of httpParams) {
-                url = url + `.replace(':${param.paramKey}', ${param.name})\n`;
-            }
-        }
-
-        return {
-            httpClientArguments: ['url'],
-            prefixCode: url,
-        }
-    }
-
-    function getEndpoint(document: Document, member: Document) {
-        let baseUrl = '';
-        if (document.decorators?.length) {
-            const [decorator] = document.decorators;
-            if (decorator && decorator.arguments?.length) {
-                baseUrl = decorator.arguments[0].name;
-            }
-        }
-
-        let finalEndpoint = baseUrl.replace(/\'/g, '');
-
-        const [decorator] = member.decorators ?? [];
-
-        if (decorator && decorator.arguments && decorator.arguments.length) {
-            const url = decorator.arguments
-                .map(argument => argument.name)
-                .join('/')
-                .replace(/\'/g, '');
-
-            finalEndpoint = `${finalEndpoint}/${url}`;
-        }
-
-        return finalEndpoint;
-    }
-
+export function schematics(options: AngularServiceSchema): Rule {
     return (_: Tree, _context: SchematicContext) => {
-        // if (!_options.document) {
-        //   throw new SchematicsException('The argument document is required!');
-        // }
-
-        _options.document = document;
-
-        const eslint = new ESLint({ fix: true });
+        if (!options.document || !Object.keys(options.document).length) {
+          throw new SchematicsException('The argument document is required!');
+        }
 
         const templateSource = apply(
             url('./files'),
@@ -238,28 +34,17 @@ export function schematics(_options: AngularServiceSchema): Rule {
                     getEndpoint,
                     buildMemberParameters,
                     buildBodyFromMember,
-                    name: _options.document.name,
-                    members: _options.document.members,
-                    document: _options.document,
+                    name: options.document.name,
+                    members: options.document.members,
+                    document: options.document,
                 }),
-                move(normalize(_options.path ?? '')),
-                forEach((fileEntry) => {
-                    setTimeout(async () => {
-                        const results = await eslint.lintFiles([`/home/felipe-pc/projects/fastty/packages/schematics/cats-controller.service.ts`]);
-                        await ESLint.outputFixes(results);
-                        const formatter = await eslint.loadFormatter('stylish');
-                        const formated = formatter.format(results);
-
-                        _.overwrite(normalize(fileEntry.path), formated);
-                    }, 100);
-    
-                    return fileEntry;
-                }),
+                move(normalize(options.path ?? '')),
+                runLint(),
             ],
         )
 
         return chain([
-            mergeWith(templateSource),
+            mergeWith(templateSource, MergeStrategy.Overwrite),
         ]);
     };
 }
