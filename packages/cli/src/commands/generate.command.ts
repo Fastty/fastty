7aa09d991b98a10b067a92971cb89a7f781720cc
Felipe-BP <gvv.fel@gmail.com>
2021-03-30 00:19:57 -0300

    feat(cli): integrate generate command with generate action

--- packages/cli/src/commands/generate.command.ts
+++ packages/cli/src/commands/generate.command.ts
@@ -1,14 +1,13 @@
 import { CommanderStatic } from 'commander';
+import { generateFilesAction } from '../actions';
 
 export function generateCommand(program: CommanderStatic) {
     program
-        .command('generate <schematic> [sourcePath]')
+        .command('generate <sourcePath>')
         .alias('g')
-        .option('-d, --dry-run', 'Report actions that would be taken without writing out results.')
+        .option('-d, --dry-run', 'Report actions that would be taken without writing out results.', false)
         .option('-p, --project [project]', 'Project in which to generate files.')
         .option('-c, --collection [collectionName]', 'Schematics collection to use.')
         .helpOption('-h, --help', 'Output usage information.')
-        .action(async (schematic: string, sourcePath: string | null) => {
-            console.info(`generate command with ${schematic} and ${sourcePath ?? ''}!`);
-        });
+        .action(generateFilesAction);
 }
