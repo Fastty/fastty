9ea09d0b5184b61a728a09a7e7ad736094084715
Felipe-BP <gvv.fel@gmail.com>
2021-06-02 22:12:47 -0300

    feat: support watch mode to auto generate on changes detected

--- packages/cli/src/actions/generate.actions.ts
+++ packages/cli/src/actions/generate.actions.ts
@@ -1,13 +1,14 @@
 import { resolve } from 'path';
 import { statSync } from 'fs';
 
-import { OptionValues } from 'commander';
 import { parseSource } from '@fastty/core';
+import { watch as watchFiles } from 'chokidar';
+import { OptionValues } from 'commander';
 import {
-    Program,
     SourceFile,
     ScriptTarget,
     CompilerOptions,
+    TypeChecker,
     getDefaultCompilerOptions,
     createProgram,
 } from 'typescript';
@@ -18,8 +19,36 @@ import { FASTTY_SCHEMATICS_PATH } from '../constants/fastty-schematics-path';
 
 interface Options extends OptionValues {
     dryRun: boolean;
+    watch: boolean;
     path: string | null;
     collection: string;
+}
+
+function getProgramFromRootFiles(files: Array<string>) {
+    const compilerOptions: CompilerOptions = {
+        ...getDefaultCompilerOptions(),
+        allowJs: true,
+        target: ScriptTarget.ES5,
+        experimentalDecorators: true,
+    };
+    return createProgram(files, compilerOptions);
+}
+
+function executeFileGeneration(sourceFile: SourceFile, binary: string, typeChecker: TypeChecker, options: Options) {
+    const { dryRun, collection, watch } = options;
+    const document = parseSource(sourceFile, typeChecker);
+
+    new CliRunner()
+        .runner(runSchematic)
+        .binary(binary)
+        .command(`${FASTTY_SCHEMATICS_PATH}:${collection ?? 'angular'}`)
+        .args(`--document='${JSON.stringify(document)}'`, `--dry-run=${dryRun}`)
+        .exec();
+
+    if (watch && !dryRun) {
+        const watcher = watchFiles(sourceFile.fileName);
+        watcher.on('change', (path) => generateFilesAction(path, { ...options, watch: false }));
+    }
 }
 
 export function generateFilesAction(sourcePath: string, options: Options) {
@@ -28,20 +57,14 @@ export function generateFilesAction(sourcePath: string, options: Options) {
     const stat = statSync(solvedPath);
 
     if (!stat.isDirectory()) {
-        const fileNames = [solvedPath];
         const importedSourceFiles: SourceFile[] = [];
-        const program = getProgramFromRootFiles(fileNames);
-        const isRootSourceFile = (fileName: string) => fileNames.some((rootPath) => rootPath === fileName);
+        const program = getProgramFromRootFiles([solvedPath]);
+        const isRootSourceFile = (fileName: string) =>
+            program.getRootFileNames().some((rootFileName) => rootFileName === fileName);
 
         program.getSourceFiles().forEach((sourceFile) => {
             if (isRootSourceFile(sourceFile.fileName)) {
-                const document = parseSource(sourceFile, program.getTypeChecker());
-                new CliRunner()
-                    .runner(runSchematic)
-                    .binary(schematicBin)
-                    .command(`${FASTTY_SCHEMATICS_PATH}:${options.collection ?? 'angular'}`)
-                    .args(`--document='${JSON.stringify(document)}'`, `--dry-run=${options.dryRun}`)
-                    .exec();
+                executeFileGeneration(sourceFile, schematicBin, program.getTypeChecker(), options);
             }
 
             if (!sourceFile.isDeclarationFile && !isRootSourceFile(sourceFile.fileName)) {
@@ -49,13 +72,4 @@ export function generateFilesAction(sourcePath: string, options: Options) {
             }
         });
     }
-}
-
-function getProgramFromRootFiles(files: Array<string>): Program {
-    const compilerOptions: CompilerOptions = {
-        ...getDefaultCompilerOptions(),
-        allowJs: true,
-        target: ScriptTarget.ES5,
-    };
-    return createProgram(files, compilerOptions);
 }
--- packages/cli/src/commands/generate.command.ts
+++ packages/cli/src/commands/generate.command.ts
@@ -6,6 +6,7 @@ export function generateCommand(program: CommanderStatic) {
         .command('generate <sourcePath>')
         .alias('g')
         .option('-d, --dry-run', 'Report actions that would be taken without writing out results.', false)
+        .option('-w, --watch', 'Watchs input files changes and auto generate results again.', false)
         .option('-p, --path [path]', 'Path that should save exported files')
         .helpOption('-h, --help', 'Output usage information.')
         .action(generateFilesAction);
